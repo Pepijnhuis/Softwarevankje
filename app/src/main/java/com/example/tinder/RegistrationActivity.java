7fd9e4b75b7eb9a6704b6a9aefedfc599f4f5809
Pepijn Huis in 't Veld <p.i.huisintveld@student.tudelft.nl>
2019-03-06 15:04:27 +0100

    Database erbij

--- app/src/main/java/com/example/tinder/RegistrationActivity.java
+++ app/src/main/java/com/example/tinder/RegistrationActivity.java
@@ -1,12 +1,16 @@
 package com.example.tinder;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,14 +18,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private Button mRegister;
 
-    private EditText mEmail,mPassword;
+    private EditText mEmail,mPassword,mName;
 
+    private RadioGroup mRadiogroup;
     private FirebaseAuth mAuth;
+
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
 
@@ -30,11 +38,13 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        Log.d("DEBUG", "Oncreate done");
 
         mAuth = FirebaseAuth.getInstance();
         firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Log.d("DEBUG", "Firebaselistener gedaan");
                 final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user !=null){
                     Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
@@ -45,20 +55,34 @@ public class RegistrationActivity extends AppCompatActivity {
         };
 
         mRegister = (Button) findViewById(R.id.register);
-
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
+        mName = (EditText) findViewById(R.id.name);
+        mRadiogroup = (RadioGroup) findViewById(R.id.radiogroup);
 
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = mEmail.getText().toString();
-                String password = mPassword.getText().toString();
+                Log.d("DEBUG", "Knop geklikt");
+                int selectId = mRadiogroup.getCheckedRadioButtonId();
+                final RadioButton radioButton = (RadioButton) findViewById(selectId);
+
+                if(radioButton.getText() == null) {
+                    return;
+                }
+                final String name = mName.getText().toString();
+                final String email = mEmail.getText().toString();
+                final String password = mPassword.getText().toString();
                 mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (!task.isSuccessful()) {
                             Toast.makeText(RegistrationActivity.this, "Signup Error", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            String userID = mAuth.getCurrentUser().getUid();
+                            DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(radioButton.getText().toString()).child(userID).child("Name");
+                            currentUserDb.setValue(name);
                         }
                     }
                 });
