206f860701e6a3e53b083499e7385f51aaddb997
yvonruitenburg <48204504+yvonruitenburg@users.noreply.github.com>
2019-03-08 12:34:39 +0100

    CreateStudent3+4

---
+++ app/src/main/java/com/example/tinder/CreateAccount3House.java
@@ -0,0 +1,13 @@
+package com.example.tinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAccount3House extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account3_house);
+    }
+}
