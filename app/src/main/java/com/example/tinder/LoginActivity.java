ed488d9450639027a6c780bdd22c963ff860b2f4
Pepijn Huis in 't Veld <p.i.huisintveld@student.tudelft.nl>
2019-03-08 12:42:09 +0100

    Fixesss

--- app/src/main/java/com/example/tinder/LoginActivity.java
+++ app/src/main/java/com/example/tinder/LoginActivity.java
@@ -37,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user !=null){
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    Intent intent = new Intent(LoginActivity.this, Slideactivity.class);
                     startActivity(intent);
                     finish();
                 }
