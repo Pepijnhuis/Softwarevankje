633ba1585d77b6ec3a90433fa4893fcbea19a0a5
yvonruitenburg <48204504+yvonruitenburg@users.noreply.github.com>
2019-03-15 10:30:51 +0100

    try1

--- app/src/main/java/com/example/tinder/LoginActivity.java
+++ app/src/main/java/com/example/tinder/LoginActivity.java
@@ -49,8 +49,7 @@ public class LoginActivity extends AppCompatActivity {
 
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCl
-            ick(View v) {
+            public void onClick(View v) {
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
                 mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
