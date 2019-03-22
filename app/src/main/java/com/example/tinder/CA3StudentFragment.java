199d8e3ea416acd7592fe9b77b5182a7210ccb34
yvonruitenburg <48204504+yvonruitenburg@users.noreply.github.com>
2019-03-22 15:06:52 +0100

    Settings en Messages update

--- app/src/main/java/com/example/tinder/CA3StudentFragment.java
+++ app/src/main/java/com/example/tinder/CA3StudentFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class CA3StudentFragment extends Fragment {
 
@@ -21,6 +23,7 @@ public class CA3StudentFragment extends Fragment {
         //Container = viewgroup that contains the fragment layout
         //Attach to root is false
         View view = inflater.inflate(R.layout.ca3_student_fragment, container, false);
+
         return view;
     }
 
--- app/src/main/java/com/example/tinder/CreateAccountStudent.java
+++ app/src/main/java/com/example/tinder/CreateAccountStudent.java
@@ -6,6 +6,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -42,7 +43,6 @@ public class CreateAccountStudent extends AppCompatActivity {
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-
     }
 
 
@@ -66,36 +66,6 @@ public class CreateAccountStudent extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    //A FragmentPageAdapter that returns a fragment corresponding to one of the tabs
-    public static class PlaceholderFragment extends Fragment {
-        //The fragment argument representing the section number for this fragment.
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.ca1_fragment, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
     }
 
     /**
