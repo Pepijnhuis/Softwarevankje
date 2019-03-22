6944335458a8fbcd97f61e548133dc9b653421ac
Thomas <t.vanmuiden@gmail.com>
2019-03-22 10:17:40 +0100

    Database update

--- app/src/main/java/com/example/tinder/CreateAccount3Student.java
+++ app/src/main/java/com/example/tinder/CreateAccount3Student.java
@@ -1,6 +1,7 @@
 package com.example.tinder;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,21 +10,27 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccount3Student extends AppCompatActivity {
 
     private EditText mNameField, mAdressField, mDayField, mMonthField, mYearField, mSchoolField,
             mStudyField, mHobby1Field, mHobby2Field, mHobby3Field, mAboutMeField;
 
-    private RadioGroup mBscMscRadioGroup;
+    private RadioGroup mRadioGroupBscMsc, mRadioGroupMaleFemale;
 
     private Button mBack, mNext, mConfirm;
 
     private FirebaseAuth mAuth;
     private DatabaseReference mStudentAccountDatabase;
-    private String Name, Adress, School, Study, Hobby1, Hobby2, Hobby3, AboutMe;
+    private String NameStudent, AdressStudent, School, Study, Hobby1, Hobby2, Hobby3, AboutMe, userId;
     private Integer Day, Month, Year;
 
     @Override
@@ -43,18 +50,64 @@ public class CreateAccount3Student extends AppCompatActivity {
         mHobby3Field = (EditText) findViewById(R.id.Hobby3);
         mAboutMeField = (EditText) findViewById(R.id.AboutMe);
 
-        mBscMscRadioGroup = (RadioGroup) findViewById(R.id.RadioGroupBscMsc);
+        mRadioGroupBscMsc = (RadioGroup) findViewById(R.id.RadioGroupBscMsc);
+        mRadioGroupMaleFemale = (RadioGroup) findViewById(R.id.RadioGroupMaleFemale);
 
-       // mBack = (Button) findViewById(R.id.NameStudent);
+        // mBack = (Button) findViewById(R.id.NameStudent);
         mNext = (Button) findViewById(R.id.nextCreateAccount3Student);
 
-       mNext.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
+        mAuth = FirebaseAuth.getInstance();
+        userId = mAuth.getCurrentUser().getUid();
+        mStudentAccountDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Students").child("UserId");
 
-           }
-       });
+        getUserInfo();
+
+        mNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                saveUserInformation();
+
+            }
+        });
 
 
     }
+
+    private void getUserInfo() {
+        mStudentAccountDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if (map.get("Name") != null) {
+                        NameStudent = map.get("Name").toString();
+                        mNameField.setText(NameStudent);
+                    }
+
+                    if (map.get("Name") != null) {
+                        AdressStudent = map.get("Adress").toString();
+                        mAdressField.setText(AdressStudent);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void saveUserInformation() {
+        NameStudent = mNameField.getText().toString();
+        AdressStudent = mAdressField.getText().toString();
+
+        Map userInfo = new HashMap();
+        userInfo.put("Name", NameStudent);
+        userInfo.put("Adress", AdressStudent);
+        mStudentAccountDatabase.updateChildren(userInfo);
+
+    }
+
 }
