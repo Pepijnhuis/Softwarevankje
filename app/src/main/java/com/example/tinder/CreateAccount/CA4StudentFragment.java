2e2e365945e0dc9ea60ca0355b78ef1fa699b6c0
Pepijn Huis in 't Veld <p.i.huisintveld@student.tudelft.nl>
2019-04-03 12:15:46 +0200

    Download url goed nog geen database

--- app/src/main/java/com/example/tinder/CreateAccount/CA4StudentFragment.java
+++ app/src/main/java/com/example/tinder/CreateAccount/CA4StudentFragment.java
@@ -1,6 +1,7 @@
 package com.example.tinder.CreateAccount;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -16,12 +17,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.tinder.R;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
@@ -33,6 +38,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 
 public class CA4StudentFragment extends Fragment {
@@ -44,8 +50,11 @@ public class CA4StudentFragment extends Fragment {
     private ImageView mProfileImage1;
     private Uri resultUri;
     private String Image1, userId; //downloadUrl;
+
     private FirebaseAuth mAuth;
     private DatabaseReference mStudentAccountDatabase;
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
 
     //Building the fragment
@@ -55,6 +64,9 @@ public class CA4StudentFragment extends Fragment {
         //Pass the layout from settings_fragment
         //Container = viewgroup that contains the fragment layout
         //Attach to root is false
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
         View view = inflater.inflate(R.layout.ca4_student_fragment, container, false);
 
         mSkip = (Button) view.findViewById(R.id.ButtonBackCA4Student);
@@ -116,9 +128,39 @@ public class CA4StudentFragment extends Fragment {
     }
 
     private void saveUserImage() {
+        if (resultUri != null){
+
+
+            StorageReference ref =  storageReference.child("images/"+ UUID.randomUUID().toString());
+
+            ref.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!urlTask.isSuccessful());
+                    Uri downloadUrl = urlTask.getResult();
+                    Log.d("Debug", downloadUrl.toString());
+
+                    userId = mAuth.getCurrentUser().getUid();
+                    mStudentAccountDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Student").child(userId);
+
+                    Map userInfo = new HashMap();
+                    userInfo.put("ProfileImageUrl", downloadUrl.toString());
+                    Log.d("Debug", String.valueOf(userInfo));
+                    mStudentAccountDatabase.updateChildren(userInfo);
+
+                    }
+                });
+
+
+
+
+
+
+
         //mStudentAccountDatabase.updateChildren();
 
-        if(resultUri != null) {
+       /* if(resultUri != null) {
             
             StorageReference filepath = FirebaseStorage.getInstance().getReference().child("Users").child("Student").child(userId);
             Bitmap bitmap = null;
@@ -144,14 +186,6 @@ public class CA4StudentFragment extends Fragment {
             uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    //taskSnapshot.getDownloadUrl is obsolete
-                    //String downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
-                    //String downloadUrl = FirebaseStorage.getInstance().getReference().getDownloadUrl().toString(); //gives wrong url
-
-                    //StorageReference downloadUrl = FirebaseStorage.getInstance().getReference(); gives wrong url
-                    //return dateRef.toString();
-
-                    //String downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString(); //gives wrong url
 
                     String downloadUrl = FirebaseStorage.getInstance().getReference().getDownloadUrl().toString();
 
@@ -168,14 +202,19 @@ public class CA4StudentFragment extends Fragment {
         }
 
         else {getActivity().finish();}
+
+        */
+
+
+
+        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
-            final Uri imageUri  = data.getData();
-            resultUri = imageUri;
+            resultUri = data.getData();;
             mProfileImage1.setImageURI(resultUri);
         }
     }
