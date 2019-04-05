9b079cd1188e603367241a2ce637de071ed66be1
yvonruitenburg <48204504+yvonruitenburg@users.noreply.github.com>
2019-04-05 12:32:45 +0200

    Login half oke

--- app/src/main/java/com/example/tinder/LoginActivity.java
+++ app/src/main/java/com/example/tinder/LoginActivity.java
@@ -24,6 +24,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -33,85 +34,85 @@ public class LoginActivity extends AppCompatActivity {
     private Button mRegisterbutton;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
-    private String UID, oppositeuser;
+    private String CurrentUserID, oppositeuser, userRegistration, oppositeUserRegistration;
+
+    private DatabaseReference usersDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        usersDb = FirebaseDatabase.getInstance().getReference().child("Users");
 
         mAuth = FirebaseAuth.getInstance();
         firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
+            //when logged in successful
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Log.d("Debug","onAuthStateChanged" );
                 final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                CurrentUserID = FirebaseAuth.getInstance().getUid();
+                Log.d("Debug, CurrentUserID",CurrentUserID );
+
                 if (user !=null) {
-                    //student database reference see if changed
-                    DatabaseReference Studentdb = FirebaseDatabase.getInstance().getReference().child("Users").child("Student");
-                    Studentdb.addChildEventListener(new ChildEventListener() {
+                    DatabaseReference userDb = usersDb.child(user.getUid());
+                    userDb.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
-                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                            //get current user id
-                            UID = user.getUid();
-                            Log.d("Debug", UID);
-                            //if current user id occurs in student database
-                            if (dataSnapshot.getKey().equals(user.getUid())) {
-                                oppositeuser = "Huis";
-                                Log.d("Debug", "Login als Student");
-                                Intent intent = new Intent(LoginActivity.this, MainNavigationStudent.class);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                oppositeuser = "Student";
-                                Log.d("Debug", "Login als Huis");
-                                Intent intent = new Intent(LoginActivity.this, MainNavigationHouse.class);
-                                startActivity(intent);
-                                finish();
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                if (dataSnapshot.child("Register").getValue() != null) {
+                                    userRegistration = dataSnapshot.child("Registration").getValue().toString();
+                                    switch (userRegistration) {
+                                        case "House":
+                                            oppositeUserRegistration = "Student";
+                                            Log.d("Debug register", userRegistration);
+                                            Intent intent1 = new Intent(LoginActivity.this, MainNavigationHouse.class);
+                                            startActivity(intent1);
+                                            break;
+                                        case "Student":
+                                            oppositeUserRegistration = "House";
+                                            Log.d("Debug register", userRegistration);
+                                            Intent intent2 = new Intent(LoginActivity.this, MainNavigationStudent.class);
+                                            startActivity(intent2);
+                                            break;
+                                    }
+                                }
                             }
+                            //getOppositeUserRegistration();
                         }
 
                         @Override
-                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
                         }
-
-                        @Override
-                        public void onChildRemoved(DataSnapshot dataSnapshot) {
-                        }
-
-                        @Override
-                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                        }
-
                     });
                 }
                 }
             };
 
-            mLogin = (Button) findViewById(R.id.loginaccount);
-
-            mEmail = (EditText) findViewById(R.id.emailbox);
-            mPassword = (EditText) findViewById(R.id.passwordbox);
+        mLogin = (Button) findViewById(R.id.loginaccount);
+        mEmail = (EditText) findViewById(R.id.emailbox);
+        mPassword = (EditText) findViewById(R.id.passwordbox);
 
         mLogin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     final String email = mEmail.getText().toString();
                     final String password = mPassword.getText().toString();
+                    Log.d("Debug, email edittext", email);
+                    Log.d("Debug, password edittext", password);
 
-                    if (email.matches("") ) {
+
+                    if (email.matches("")) {
                         // Show Error on edittext
                         mEmail.setError("Invalid email");
-                        Log.d("Debug", "no email");
+                        Log.d("Debug", "empty email");
 
                         //invalid email and password
                         if (password.matches("")) {
                             // Show Error on edittext
                             mPassword.setError("Invalid password");
-                            Log.d("Debug", "no password");}
+                            Log.d("Debug", "empty password");}
                         return;
                     }
 
@@ -119,29 +120,27 @@ public class LoginActivity extends AppCompatActivity {
                     if (password.matches("")) {
                         // Show Error on edittext
                         mPassword.setError("Invalid password");
-                        Log.d("Debug", "no password");
+                        Log.d("Debug", "empty password");
                         return;
                     }
+
+
                     mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
 
-                            //when login is successful
+                            //when login is not successful
                             if (!task.isSuccessful()) {
                                 Toast.makeText(LoginActivity.this, "Signin Error", Toast.LENGTH_SHORT).show();
-                            }
-                            //when login is not successful
-                            else{
                                 mEmail.setError("Invalid email");
-                                Log.d("Debug", "no email");
+                                Log.d("Debug", "invalid email");
                                 mPassword.setError("Invalid password");
-                                Log.d("Debug", "no password");
+                                Log.d("Debug", "invalid password");
                             }
 
-                            //    String userID = mAuth.getCurrentUser().getUid();
-                            //    DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userID).child("Student");
-                            //    currentUserDb.setValue(name);
-
+                            else{
+                                Log.d("Debug", "successful login");
+                            }
                         }
                     });
                 }
