f839195117610c2ad7af8d3f9f602616d1181e53
Thomas <t.vanmuiden@gmail.com>
2019-04-12 17:07:08 +0200

    Logotjes bij kameringomeuk en nog wat meer shit ik ben er helemaal klaar mee vo

--- app/src/main/java/com/example/tinder/MainNavigation/MainNavigationHouse.java
+++ app/src/main/java/com/example/tinder/MainNavigation/MainNavigationHouse.java
@@ -2,6 +2,8 @@ package com.example.tinder.MainNavigation;
 
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.app.Fragment;
@@ -86,6 +88,7 @@ public class MainNavigationHouse extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
+
             Fragment fragment = null; //setting the fragment to null initially
             switch (position){
                 case 0:
